afa3a1cd0f563257cdf4d7ca2f645349c2e7b2ba
Stephanie <stephanie7546@hotmail.com>
2022-02-07 16:34:05 +0800

    Que04
    移除新增景點至下拉式清單時可偵測重複不新增執行結果

---
+++ Que04/Default.aspx.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Que04
+{
+    public partial class Default : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void insert1_Click(object sender, EventArgs e)
+        {
+            string insert_value = TextBox1.Text;
+            DropDownList1.Items.Add(new ListItem(insert_value, insert_value));
+            Label1.Text = "新增景點：" + insert_value;
+        }
+
+        protected void delete1_Click(object sender, EventArgs e)
+        {
+            string delete_value = DropDownList1.SelectedValue;
+            DropDownList1.Items.Remove(delete_value);
+            Label1.Text = "移除景點：" + delete_value;
+        }
+
+    }
+}
