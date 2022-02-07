fd0564c7d3b3f5c0f4f238885f2df33d3fa48826
Stephanie <stephanie7546@hotmail.com>
2022-02-07 16:44:08 +0800

    Que04
    移除新增景點至下拉式清單時可偵測重複不新增執行結果

---
+++ Que04/Default.aspx
@@ -0,0 +1,29 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Default.aspx.cs" Inherits="Que04.Default" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            旅遊景點：
+            <asp:DropDownList ID="DropDownList1" runat="server">
+            </asp:DropDownList>
+        </div>
+        <div>
+            新增旅遊景點：
+            <asp:TextBox ID="TextBox1" runat="server"></asp:TextBox>
+        </div>
+        <div>
+            <asp:Button ID="insert1" runat="server" Text="新增" OnClick="insert1_Click"/>
+            <asp:Button ID="delete1" runat="server" Text="移除" OnClick="delete1_Click"/>
+        </div>
+        <asp:Label ID="Label1" runat="server" Text=""></asp:Label>
+    </form>
+
+</body>
+</html>
---
+++ Que04/Default.aspx.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Que04
+{
+    public partial class Default : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void insert1_Click(object sender, EventArgs e)
+        {
+            string insert_value = TextBox1.Text;
+
+            ListItem listItem = new ListItem(insert_value, insert_value);
+            if (DropDownList1.Items.Contains(listItem))
+            {
+                Label1.Text = "新增景點：" + insert_value + "已存在";
+            }
+            else
+            {
+                DropDownList1.Items.Add(listItem);
+                Label1.Text = "新增景點：" + insert_value;
+            }
+        }
+
+        protected void delete1_Click(object sender, EventArgs e)
+        {
+            string delete_value = DropDownList1.SelectedValue;
+            DropDownList1.Items.Remove(delete_value);
+            Label1.Text = "移除景點：" + delete_value;
+        }
+
+    }
+}
